0b5133099e5d865fe1423b6766e27ecfb3051d96
Vinícius Albuquerque <vinicius.albuquerque@aevo.com.br>
2019-08-07 13:35:38 -0300

    feat: backend fully created, integrating with mongo database

---
+++ backend/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes");
+
+const server = express();
+
+mongoose.connect(
+  "mongodb+srv://livre:livreacesso@cluster0-clgs7.mongodb.net/tindev?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
+
+server.use(cors());
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
